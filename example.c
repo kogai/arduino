50cea709afcec31f7c31bf7b97222228255747d8
小飼慎一 <kogai0121@gmail.com>
2014-12-20 23:34:34 +0900

    青色ダイオード&&アナログ出力テスト

--- example.c
+++ example.c
@@ -1,19 +1,25 @@
 const int LED = 13;
-const int LED2 = 12;
+const int LED2 = 9;
 const int BUTTON = 7;
 
-int val = 0;
+int i = 0;
 
 void setup(){
+	Serial.begin(9600);
 	pinMode(LED, OUTPUT);
 	pinMode(LED2, OUTPUT);
-	digitalWrite(LED, HIGH);
-	// pinMode(BUTTON, INPUT);
+	pinMode(BUTTON, INPUT);
 }
 
 void loop(){
-	digitalWrite(LED2, HIGH);
+	analogWrite(LED2, 50);
 	delay(1);
 	digitalWrite(LED2, LOW);
 	delay(20);
+	Serial.println(digitalRead(BUTTON));
+	// for ( i = 0; i < 255; ++i)
+	// {
+	// 	analogWrite(LED2, i);
+	// 	delay(25);
+	// }
 }
